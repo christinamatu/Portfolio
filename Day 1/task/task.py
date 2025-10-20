431ada113f187a4ac9532763dc5569a0d12dec44
Christina Matu <160041864+christinamatu@users.noreply.github.com>
2025-10-19 20:18:09 -0400

    Delete Day 1 directory

--- Day 1/task/task.py
+++
@@ -1,4 +0,0 @@
-print("Hello " + input("What is your name? \n") + ", welcome to the Band Name Generator!")
-city = input("What city did you grow up in? \n")
-pet = input("What is the name of your pet? \n")
-print("The name of your band is: " + city + " " + pet)
